3fefa069837bf1097f84455869deb576799a2fbb
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2024-01-29 13:26:15 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,30 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ $registrations }}
+                <div class="p-6 text-gray-900 flex flex-col">
+                    @foreach ($registrations as $registration)
+                    <div class="flex flex-row justify-between">
+                        <div class="flex flex-col">
+                            <div class="text-lg font-bold">
+                                {{ $registration->firstname }} {{ $registration->lastname }}
+                            </div>
+                            <div class="text-sm">{{ $registration->email }}</div>
+                        </div>
+                    </div>
+                    @endforeach
+                    <!-- emails to copy -->
+                    <div class="flex flex-row justify-between">
+                        <div class="flex flex-col">
+                            <div class="text-lg font-bold">
+                                Emails to copy
+                            </div>
+                            <div class="text-sm">
+                                @foreach ($registrations as $registration)
+                                {{ $registration->email }};
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
